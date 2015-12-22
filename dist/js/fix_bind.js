700b1d7d097f7aa0f897d71e1f71382e350f1085
mcoenca <m.coenca@gmail.com>
2015-12-22 15:20:48 +0100

    bind is fixed, phantom working

--- dist/js/fix_bind.js
+++ dist/js/fix_bind.js
@@ -1,7 +1,7 @@
 if (!Function.prototype.bind) {
   Function.prototype.bind = function() {
     var args, oThis;
-    oThis = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-    return _.bind.apply(_, [this, oThis].concat(slice.call(args)));
+    oThis = arguments[0], args = 2 <= arguments.length ? Array.prototype.slice.call(arguments, 1) : [];
+    return _.bind.apply(_, [this, oThis].concat(Array.prototype.slice.call(args)));
   };
 }
--- dist/js/materialize.js
+++ dist/js/materialize.js
@@ -1527,8 +1527,8 @@ $(document).ready(function(){
     'use strict';
     Function.prototype.bind = function() {
       var args, oThis;
-      oThis = arguments[0], args = 2 <= arguments.length ? slice.call(arguments, 1) : [];
-      return _.bind.apply(_, [this, oThis].concat(slice.call(args)));
+      oThis = arguments[0], args = 2 <= arguments.length ? Array.prototype.slice.call(arguments, 1) : [];
+      return _.bind.apply(_, [this, oThis].concat(Array.prototype.slice.call(args)));
     };
     var Waves = Waves || {};
     var $$ = document.querySelectorAll.bind(document);
